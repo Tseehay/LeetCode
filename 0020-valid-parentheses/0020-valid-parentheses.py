b3242ed82fed04a08ba7a8bf1a69a803ba9133f7
Tsehay Goremes <118212771+Tseehay@users.noreply.github.com>
2024-08-07 20:12:49 +0300

    Time: 25 ms (97.57%), Space: 16.5 MB (87.32%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -6,9 +6,10 @@ class Solution:
         for char in s:
             if char in mapping:
                 top_element = stack.pop() if stack else '#'
+                
                 if mapping[char] != top_element:
                     return False
             else:
                 stack.append(char)
-
+                
         return not stack
