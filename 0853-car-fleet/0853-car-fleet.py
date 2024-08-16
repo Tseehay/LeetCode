5747d7b0799f184c94af48d92a8c1614baa6f54f
Tsehay Goremes <118212771+Tseehay@users.noreply.github.com>
2024-08-16 19:16:51 +0300

    Time: 679 ms (63.67%), Space: 38.9 MB (40.56%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,10 +1,14 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        ans = 0
-        pacer = -sys.maxsize
-        for p, s in sorted(zip(position, speed), reverse=True): 
-            v = (target - p)  / s
-            if v > pacer:
-                pacer = v 
-                ans += 1
-        return ans 
+        dSpeed = []
+        stack = []
+        for i in range(len(position)):
+            dSpeed.append((position[i],speed[i]))
+        dSpeed = sorted(dSpeed, reverse=True)
+        
+        for d, speed in dSpeed:
+            distance = target-d
+            if not stack or (distance/speed)>stack[-1]:
+                stack.append(distance/speed)
+                
+        return len(stack)
